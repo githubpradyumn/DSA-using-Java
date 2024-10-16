fbcdbbee2b485aff28208a8533d8744605ad2dc2
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-10-16 23:52:08 +0530

    add to git

--- 02.Linear Search/solution.java
+++ 02.Linear Search/solution.java
@@ -1,7 +1,9 @@
 
-import java.util.Arrays;
+import java.awt.datatransfer.FlavorListener;
 
-public class solution{
+
+
+// public class solution{
     // public static void main(String[] args) {
 
         // int[] nums = {1,2,3,4,5,6,7,8,9};
@@ -98,5 +100,51 @@ public class solution{
     //     return new int[]{-1, -1};
     // }
 
-    
-}
+
+    //    // Given an array nums of integers, return how many of them contain an even number of digits.
+
+        
+//
+    //    // Example 1:
+//
+    //    // Input: nums = [12,345,2,6,7896]
+    //    // Output: 2
+    //    // Explanation: 
+    //    // 12 contains 2 digits (even number of digits). 
+    //    // 345 contains 3 digits (odd number of digits). 
+    //    // 2 contains 1 digit (odd number of digits). 
+    //    // 6 contains 1 digit (odd number of digits). 
+    //    // 7896 contains 4 digits (even number of digits). 
+    //    // Therefore only 12 and 7896 contain an even number of digits.
+
+    class Solution {
+        public int findNumbers(int[] nums) {
+            int count = 0 ;
+            for (int num : nums) {
+                if (even(num)){
+                    count++;
+                }
+            }
+            return count;
+        }
+        static boolean even(int num){
+            int numberofdigit = digits(num);
+            if (numberofdigit%2==0){
+                return true;
+            }
+            return false;
+        }
+        static int digits(int num){
+            int count = 0;
+            while (num > 0 ){
+                count++ ;
+                num = num/10;
+            }
+            return count;
+        }
+        public static void main(String[] args) {
+            int nums = [12,345,2,6,7896];
+
+        }
+    }
+
