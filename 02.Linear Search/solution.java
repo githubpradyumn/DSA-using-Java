01038618e1512789d8f891c8e13bc986088cc8b8
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-10-20 20:09:17 +0530

    add to git

--- 02.Linear Search/solution.java
+++ 02.Linear Search/solution.java
@@ -1,108 +1,33 @@
 
-import java.awt.datatransfer.FlavorListener;
+// import java.lang.reflect.Array;
 
+// Simple Linear Search Code 
+// class solution {
+//     public static void main(String[] args) {
+//         int[] nums = {1,2,3,4,5,6,7,8,9,0};
+//         int target = 7;
+//         int ans = LinearSearch(nums,target);
+//         System.out.println(ans);
+//     }
 
+//     static int LinearSearch(int[] arr,int target){
+//         if (arr.length==0){
+//             return -1 ;
+//         }
+//         for (int index = 0; index < arr.length; index++) {
+//             int element = arr[index];
+//             if (target==element){
+//                 return index;
+//             }
+//         }
+//     return -1;
+//     }
 
-// public class solution{
-    // public static void main(String[] args) {
+// }
 
-        // int[] nums = {1,2,3,4,5,6,7,8,9};
-        // int target = 56;
-        // // // Linear Search
-        // int ans = linearSearch(nums, target);
-        // System.out.println(ans);
-        // // // Search In range
-        // int start = 1;
-        // int end = 5;
-        // System.out.println(SearchInRange(nums, target, start, end));
-        // // Max And Min
-        // System.out.println(Max(nums));
-        
-        
-    // }
-        
-    // // // Linear Search
-    // static int linearSearch(int[] arr , int target ) {
-        
-    //     if (arr.length==0){
-    //         return -1;
-    //     }
-
-    //     for (int index = 0; index < arr.length; index++) {
-    //         int element = arr[index];
-    //         if (element == target) {
-    //             return index;
-    //             // // or we can return the element or boolean value
-    //         }
-    //     }
-    //     return -1;
-    // }
-     
-    // // Search in Range 
-    // static int SearchInRange(int[] arr , int target , int start , int end ) {
-        
-    //     if (arr.length==0){
-    //         return -1;
-    //     }
-
-    //     for (int index = start; index < end; index++) {
-    //         int element = arr[index];
-    //         if (element == target) {
-    //             return index;
-    //             // // or we can return the element or boolean value
-    //         }
-    //     }
-    //     return -1; 
-    // }
-
-    // // Finding Max and min
-    // static int Max(int[] arr){
-        // int max_number = arr[0];
-        // for (int index = 1; index < arr.length; index++) {
-    //         if (max_number <= arr[index]){
-    //             max_number = arr[index];
-    //         }
-    //     }
-    //     return max_number;
-    // }
-
-    
-    // public static void main(String[] args) {
-    //     // Define a 2D array of integers
-    //     int[][] nums = {
-    //         {1, 2, 3, 4},
-    //         {5, 6, 7, 8, 9, 20},
-    //         {11, 23, 45, 67}
-    //     };
-        
-    //     // Define the target value to search for
-    //     int target = 8;
-        
-    //     // Call the search method and store the result
-    //     int[] ans = search2DArr(nums, target);
-        
-    //     // Print the result as a string
-    //     System.out.println(Arrays.toString(ans));
-    // }
-    // static int[] search2DArr(int[][] arr, int target) {
-    //     // Iterate through each row of the 2D array
-    //     for (int row = 0; row < arr.length; row++) {
-    //         // Iterate through each column of the current row
-    //         for (int col = 0; col < arr[row].length; col++) {
-    //             // Check if the current element matches the target
-    //             if (target == arr[row][col]) {
-    //                 // If found, return the current indices
-    //                 return new int[]{row, col};
-    //             }
-    //         }
-    //     }
-    //     // If target is not found, return {-1, -1}
-    //     return new int[]{-1, -1};
-    // }
-
-
-    //    // Given an array nums of integers, return how many of them contain an even number of digits.
-
+// // Search in Range 
+ // Finding Max and min
+ /    // Given an array nums of integers, return how many of them contain an even number of digits.
         
 //
     //    // Example 1:
@@ -116,35 +41,3 @@ import java.awt.datatransfer.FlavorListener;
     //    // 6 contains 1 digit (odd number of digits). 
     //    // 7896 contains 4 digits (even number of digits). 
     //    // Therefore only 12 and 7896 contain an even number of digits.
-
-    class Solution {
-        public int findNumbers(int[] nums) {
-            int count = 0 ;
-            for (int num : nums) {
-                if (even(num)){
-                    count++;
-                }
-            }
-            return count;
-        }
-        static boolean even(int num){
-            int numberofdigit = digits(num);
-            if (numberofdigit%2==0){
-                return true;
-            }
-            return false;
-        }
-        static int digits(int num){
-            int count = 0;
-            while (num > 0 ){
-                count++ ;
-                num = num/10;
-            }
-            return count;
-        }
-        public static void main(String[] args) {
-            int nums = [12,345,2,6,7896];
-
-        }
-    }
-
--- 03.Binary Search/solution.java
+++ 03.Binary Search/solution.java
@@ -1,35 +1,28 @@
-public class solution {
+class solution{
     public static void main(String[] args) {
-
-        int[] num = {-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10};
-        int target = 2 ;
-        int start = 0;
-        int end = num.length - 1 ; 
-        int ans = BinarySearch(num, target, start, end);
+        int[] nums = {-10,-5,0,5,6,9,12,13,14,15,16,34,46,75,100};
+        int target = 0;
+        int Start = 0;
+        int End = nums.length - 1 ;
+        int ans = BinarySearch(nums, target, Start, End);
         System.out.println(ans);
-        
     }
 
-    static int BinarySearch(int[] arr , int target , int start , int end ){
-
-
-        while (start <= end ){
-            int mid = start + (end - start)/2 ;
-            
-            if (target > arr[mid]) {
-
-                start = mid + 1;
-
-            } else if (target < arr[mid]) {
-
-                end = mid -1;
-
-            } else {
-                return mid ;
-            }
+    static int BinarySearch(int[] arr , int target , int Start , int End){
+    if (arr.length==1){
+        return arr[0];
+    }
+    while ( Start <= End ){
+        int mid = Start + (End - Start)/2;
+        if (arr[mid] < target){
+            Start = mid + 1;
+        } else if (arr[mid] > target) {
+            End = mid - 1;
+        } else {
+            return mid;
         }
-        return -1;
-
     }
-    
+    return -1; 
+    }
 }
+
