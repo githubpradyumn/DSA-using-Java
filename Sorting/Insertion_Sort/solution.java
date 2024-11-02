929f7084500ddb534dcc35b1d3c0f79b65428800
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-02 15:05:45 +0530

    add to git

--- Sorting/Insertion_Sort/solution.java
+++ Sorting/Insertion_Sort/solution.java
@@ -1,7 +1,26 @@
-// package DSA-using-Java.Sorting.Insertion_Sort;
-
+package DSA-using-Java.Sorting.Insertion_Sort;
+import java.util.Arrays;
 public class solution {
-    
-    
-
+    public static void main(String[] args) {
+        int[] arr = {3,6,8,2,9,1,5};
+        Insertion_sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void Insertion_sort(int[] arr){
+        for (int idx = 0; idx < arr.length-1; idx++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]){
+                    swap(arr,j,j-1);
+                } else {
+                    break
+                }
+            }
+            
+        }
+    }
+    static void swap(int[] arr, int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
