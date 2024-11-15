6fe815293e96dd5f6c30e64d1bd8925708438948
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-15 15:08:01 +0530

    modify in code

---
+++ 04.Recursion/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static void main(String[] args) {
+        message();
+    }
+    
+    static void message() {
+        System.out.println("Hello World!!!!");
+    }
+
+}
