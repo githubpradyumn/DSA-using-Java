0af93b8b2de9a5593e5b072af463515388fe0911
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-15 15:38:43 +0530

    add to git

--- 02.Linear Search/solution.java
+++ 02.Linear Search/solution.java
@@ -1,7 +1,6 @@
 
-import java.lang.reflect.Array;
 
-Simple Linear Search Code 
+// Simple Linear Search Code 
 class solution {
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,6,7,8,9,0};
