3f03b6cc4adcefef60bd890221d81cde375ef8d0
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-15 19:36:29 +0530

    add to git

--- 04.Recursion/Solution.java
+++ 04.Recursion/Solution.java
@@ -1,10 +1,49 @@
 public class Solution {
-    public static void main(String[] args) {
-        message();
-    }
+    // public static void main(String[] args) {
+    //     message();
+    // }
+    // static void message (){
+    //     System.out.println("Hello World!!!!!");
+    //     message1();
+    // }
+    // static void message1 (){
+    //     System.out.println("Hello World!!!!!");
+    //     message2();
+    // }
+    // static void message2 (){
+    //     System.out.println("Hello World!!!!!");
+    //     message3();
+    // }
+    // static void message3 (){
+    //     System.out.println("Hello World!!!!!");
+    //     message4();
+    // }
+    // static void message4 (){
+    //     System.out.println("Hello World!!!!!");
+    // }
     
-    static void message() {
-        System.out.println("Hello World!!!!");
-    }
 
+    // print 1 2 3 4 5
+    // public static void main(String[] args ){
+    //     Number(1);
+    // }
+    // static void Number(int n) {
+    //     System.out.println(1);
+    //     Number1(n+ 1);
+    // }
+    // static void Number1(int n ) {
+    //     System.out.println(n);
+    //     Number2(n+ 1);
+    // }
+    // static void Number2(int n ) {
+    //     System.out.println(n);
+    //     Number3(n+ 1);
+    // }
+    // static void Number3(int n ) {
+    //     System.out.println(n);
+    //     Number4(n+ 1);
+    // }
+    // static void Number4(int n ) {
+    //     System.out.println(n);
+    // }
 }
