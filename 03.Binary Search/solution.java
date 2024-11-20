00efa36177a389ee3516462ff9d71b14a4b0f55f
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-20 23:16:47 +0530

    add to git

--- 03.Binary Search/solution.java
+++ 03.Binary Search/solution.java
@@ -8,6 +8,31 @@ class solution {
         System.out.println(ans);
     }
 
+    // public static void main(String[] args ){
+    //     int[] arr = {1,2,3,4,5,6,7,8,9};
+    //     int target = 8;
+    //     int start = 0;
+    //     int end = arr.length - 1;
+    //         int i = BinarySearch(arr, target, start, end);
+    //         System.out.println(i);
+    //     }
+    
+    //     static int BinarySearch(int[] arr , int target , int start , int end ) {
+    
+    //         while (start <=  end ) {
+    //             int mid = start + (end - start) / 2;
+    //             if (arr[mid] == target) {
+    //                 return mid;
+    //             } else if (arr[mid] > target) {
+    //                 end = mid - 1;
+    //             } else {
+    //                 start = mid + 1;
+    //             }
+    //         }
+    //         return -1;
+    //     }
+    
+    // }
     static int BinarySearch(int[] arr , int target , int Start , int End){
     if (arr.length==1){
         return arr[0];
--- 04.Recursion/Solution.java
+++ 04.Recursion/Solution.java
@@ -46,4 +46,17 @@ public class Solution {
     // static void Number4(int n ) {
     //     System.out.println(n);
     // }
+
+    // its alternative 
+    public static void main (String[] args ){
+        print(1);
+    }
+    static void print(int n ){
+        if (n<=5){
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+
 }
