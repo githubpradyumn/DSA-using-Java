9563361d5c8760eefb1dd51de20b1b0148c3a7be
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-20 23:17:07 +0530

    add t ogit

--- 03.Binary Search/solution.java
+++ 03.Binary Search/solution.java
@@ -8,7 +8,7 @@ class solution {
         System.out.println(ans);
     }
 
-    // public static void main(String[] args ){
+   /* // public static void main(String[] args ){
     //     int[] arr = {1,2,3,4,5,6,7,8,9};
     //     int target = 8;
     //     int start = 0;
@@ -32,7 +32,7 @@ class solution {
     //         return -1;
     //     }
     
-    // }
+    // }*/
     static int BinarySearch(int[] arr , int target , int Start , int End){
     if (arr.length==1){
         return arr[0];
