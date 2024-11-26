f9ce740013ca34af91a79c49091e7c9dfb52220f
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-26 16:53:33 +0530

    add to git

---
+++ Sorting/Bubble_Sort/BubbleSort.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args){
+        int[] arr = {3,1,5,4,2};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void sort(int[] arr){
+        boolean swapped;
+//        i is for the outer loop
+//        run for the n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+//            j is for the inner loop
+//            max item will come at the last respective index
+            for (int j = 1; j < arr.length-i; j++) {
+//                now compare and swap the values 
+                if (arr[j]<arr[j-1]){
+            //    swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped){
+                break;
+            }
+        }
+    }
+} 
