020aaa8261dd1268f6b9bd89f0bdb4fcc7900e6e
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-26 20:52:38 +0530

    add to git

--- Sorting/Selection_Sort/solution.java
+++ Sorting/Selection_Sort/solution.java
@@ -3,33 +3,29 @@ import java.util.Arrays;
 class solution {
     
     public static void main(String[] args) {
-        int[] arr = {6,5,2,8,9,4};
-        int ans = Selection_Sort(arr);
-        // System.out.println(Selection_Sort(arr));
+        int[] arr = {3,1,5,2,7,4,6};
+        Selection(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    
-    static void Selection_Sort(int[] arr){
+    static void Selection(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             int last = arr.length - 1 - i;
-            int MaxIndex = getMaxIndex(arr, 0, last);
-            swap(arr, MaxIndex, last);
+            int getmaxindex = Max(arr,0,last);
+            swap(arr,getmaxindex,last);
         }
     }
-    
-    static void swap(int[] arr, int first , int second){
+    static int Max(int[] arr, int start , int end){
+        int max = start;
+        for (int i = start; i < end; i++) {
+            if (arr[max] < arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+    static void swap(int[] arr, int first, int second){
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
-    }
-    
-    static int getMaxIndex(int[] arr , int start , int end ){
-        int max = start;
-        for (int i = start; i <= end; i++) {
-            if (arr[max] < arr[i]){
-                max = i ;
-            }
-        }
-    return max;   
-    }
-    }
-
+    }    
+}
