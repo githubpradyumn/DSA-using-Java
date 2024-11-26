71f2a9354fb93e919ff358761749a1c62a3fde45
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-26 23:32:23 +0530

    add to git

--- 03.Binary Search/solution.java
+++ 03.Binary Search/solution.java
@@ -8,55 +8,55 @@ class solution {
         System.out.println(ans);
     }
 
-   /* // public static void main(String[] args ){
-    //     int[] arr = {1,2,3,4,5,6,7,8,9};
-    //     int target = 8;
-    //     int start = 0;
-    //     int end = arr.length - 1;
-    //         int i = BinarySearch(arr, target, start, end);
-    //         System.out.println(i);
-    //     }
-    
-    //     static int BinarySearch(int[] arr , int target , int start , int end ) {
-    
-    //         while (start <=  end ) {
-    //             int mid = start + (end - start) / 2;
-    //             if (arr[mid] == target) {
-    //                 return mid;
-    //             } else if (arr[mid] > target) {
-    //                 end = mid - 1;
-    //             } else {
-    //                 start = mid + 1;
-    //             }
-    //         }
-    //         return -1;
-    //     }
-    
-    // }*/
-    static int BinarySearch(int[] arr , int target , int Start , int End){
-    if (arr.length==1){
-        return arr[0];
-    }
-    while ( Start <= End ){
-        int mid = Start + (End - Start)/2;
-        
-        if(arr[Start]>arr[End]){
-            if (arr[mid] > target){
-                Start = mid + 1;
-            } else if (arr[mid] < target) {
-                End = mid - 1;
-            }
-        }else if (arr[Start]<arr[End]) {
-            if (arr[mid] < target){
-                Start = mid + 1;
-            } else if (arr[mid] > target) {
-                End = mid - 1;
-            }   
-        } else {
-            return mid;
+    public static void main(String[] args ){
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int target = 8;
+        int start = 0;
+        int end = arr.length - 1;
+            int i = BinarySearch(arr, target, start, end);
+            System.out.println(i);
         }
+    
+        static int BinarySearch(int[] arr , int target , int start , int end ) {
+    
+            while (start <=  end ) {
+                int mid = start + (end - start) / 2;
+                if (arr[mid] == target) {
+                    return mid;
+                } else if (arr[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            return -1;
+        }
+    
     }
-    return -1; 
-}
+//     static int BinarySearch(int[] arr , int target , int Start , int End){
+//     if (arr.length==1){
+//         return arr[0];
+//     }
+//     while ( Start <= End ){
+//         int mid = Start + (End - Start)/2;
+        
+//         if(arr[Start]>arr[End]){
+//             if (arr[mid] > target){
+//                 Start = mid + 1;
+//             } else if (arr[mid] < target) {
+//                 End = mid - 1;
+//             }
+//         }else if (arr[Start]<arr[End]) {
+//             if (arr[mid] < target){
+//                 Start = mid + 1;
+//             } else if (arr[mid] > target) {
+//                 End = mid - 1;
+//             }   
+//         } else {
+//             return mid;
+//         }
+//     }
+//     return -1; 
+// }
 }
 
--- Leedcode/solution.java
+++ Leedcode/solution.java
@@ -88,4 +88,60 @@ public class solution {
         //        }
         //        return search(nums, target, m + 1, e);
         //    }
+
+        // Question 35. Search Insert Position
+        //            Given a sorted array of distinct integers and a target value,
+        //            return the index if the target is found. If not,
+        //            return the index where it would be if it were inserted in order.
+        //
+        //    You must write an algorithm with O(log n) runtime complexity.
+        //
+        //
+        //
+        //    Example 1:
+        //
+        //    Input: nums = [1,3,5,6], target = 5
+        //    Output: 2
+        //    Example 2:
+        //
+        //    Input: nums = [1,3,5,6], target = 2
+        //    Output: 1
+        //    Example 3:
+        //
+        //    Input: nums = [1,3,5,6], target = 7
+        //    Output: 4
+        //
+        //
+        //    Constraints:
+        //
+        //            1 <= nums.length <= 104
+        //            -104 <= nums[i] <= 104
+        //             nums contains distinct values sorted in ascending order.
+        //            -104 <= target <= 104
+
+
+        // code
+        //    public static void main(String[] args ){
+        //        int[] arr = {1,3,5,6};
+        //        int target = 4;
+        //        int ans01 = binarysearch(arr, target);
+        //        System.out.println(ans01);
+        //    }
+        //    static int binarysearch(int[] nums,int target){
+        //        int start = 0;
+        //        int end = nums.length - 1;
+        //        while (start <= end) {
+        //                // as middle value changes we place it inside the loop
+        //                int mid =  start + (end-start)/2;
+        //                if(nums[mid]==target){
+        //                    return mid;
+        //                } else if(target<nums[mid]) {
+        //                    end = mid -1;
+        //                } else {
+        //                    start = mid+1;
+        //                }
+        //        }
+        //        return start;
+        //    }
+
 }
