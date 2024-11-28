50155910c1ba0719f6bf2823a05d7ba9c298c26c
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-11-28 16:27:05 +0530

    add to git

---
+++ 04.Recursion/BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main (String[] args){
+        int[] arr = {5,3,4,2,1};
+        sort(arr,arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] arr , int n){
+        if(n==1){
+            return;
+        } else {
+            for (int i = 0; i < arr.length - 1 ; i++) {
+                if (arr[i] > arr[i+1]){
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+         sort(arr,n-1);
+    }
+}
