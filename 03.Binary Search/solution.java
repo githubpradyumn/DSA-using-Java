a4639b6f48479bfd0baaad6e31d1c9457f551bc0
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-12-11 15:07:44 +0530

    Add to git

--- 03.Binary Search/solution.java
+++ 03.Binary Search/solution.java
@@ -24,31 +24,31 @@ class solution {
             return -1;
         }
     
-    }
-//     static int BinarySearch(int[] arr , int target , int Start , int End){
-//     if (arr.length==1){
-//         return arr[0];
-//     }
-//     while ( Start <= End ){
-//         int mid = Start + (End - Start)/2;
+    // }
+    // static int BinarySearch(int[] arr , int target , int Start , int End){
+    // if (arr.length==1){
+    //     return arr[0];
+    // }
+    // while ( Start <= End ){
+    //     int mid = Start + (End - Start)/2;
         
-//         if(arr[Start]>arr[End]){
-//             if (arr[mid] > target){
-//                 Start = mid + 1;
-//             } else if (arr[mid] < target) {
-//                 End = mid - 1;
-//             }
-//         }else if (arr[Start]<arr[End]) {
-//             if (arr[mid] < target){
-//                 Start = mid + 1;
-//             } else if (arr[mid] > target) {
-//                 End = mid - 1;
-//             }   
-//         } else {
-//             return mid;
-//         }
-//     }
-//     return -1; 
-// }
-
+    //     if(arr[Start]>arr[End]){
+    //         if (arr[mid] > target){
+    //             Start = mid + 1;
+    //         } else if (arr[mid] < target) {
+    //             End = mid - 1;
+    //         }
+    //     }else if (arr[Start]<arr[End]) {
+    //         if (arr[mid] < target){
+    //             Start = mid + 1;
+    //         } else if (arr[mid] > target) {
+    //             End = mid - 1;
+    //         }   
+    //     } else {
+    //         return mid;
+    //     }
+    // }
+    // return -1; 
+    // }
+}
 
