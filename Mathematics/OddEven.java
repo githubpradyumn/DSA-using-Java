f7321fc88acc53d0a1439417b44adf2443f506d5
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2024-12-21 14:29:56 +0530

    add to git

--- Mathematics/OddEven.java
+++ Mathematics/OddEven.java
@@ -2,6 +2,11 @@ public class OddEven {
     public static void main(String[] args) {
         int n = 7;
         System.out.println(ans(n));
+        // if (n%2==0){
+        //     System.out.println("No. is even");
+        // } else {
+        //     System.out.println("No. is odd ");
+        // }
     }
     static boolean ans (int n){
         return (n & 1) == 1 ;
